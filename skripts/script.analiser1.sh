ab853f13fbdb528a5e31a7f2b6b3a8ba8e3fb715
Олег <prostomolotov.os@edu.spbstu.ru>
2025-11-09 10:48:51 +0300

    Update and rename script to script.analiser1.sh

---
+++ skripts/script.analiser1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in $(ls $1)
+do
+  for file in $(cat $1$i)
+  do
+    name=$(echo $file | cut -d ":" -f1)
+    age=$(echo $file | cut -d ":" -f2)
+    email=$(echo $file | cut -d ":" -f3)
+    echo -e "Имя: $name, Возраст: $age, Почта: $email"
+  done
+done
